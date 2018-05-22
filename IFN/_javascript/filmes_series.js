87c2aa92661ad7bfa522bde6e8abe86b9d705d92
Guilherme Francisco <guisanfrancisco@gmail.com>
2018-05-22 00:23:58 -0300

    Add files via upload

--- IFN/_javascript/filmes_series.js
+++ IFN/_javascript/filmes_series.js
@@ -1,4 +1,19 @@
-function abrirSinopse(capa) {
-	var idFilho = capa.children[0].id;
-	alert(idFilho);
-}
+$(document).ready(function(){
+
+	// $("#menu-sinopse").hide();
+
+   // jQuery methods go here...
+   $("#fechar-sinopse").click(function() {
+   		$("#menu-sinopse").fadeOut("slow");
+   });
+
+   $(".capa").click(function() {
+   		$("#menu-sinopse").hide();
+   		var titulo = $(this).find('h2').text();
+   		var conteudo = $(this).find('p').text();
+   		$("#menu-sinopse").find('h2').html(titulo);
+   		$("#menu-sinopse").find('p').html(conteudo);
+   		$("#menu-sinopse").fadeIn("slow");
+   });
+
+});
